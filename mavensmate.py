4edbb33869fc5288cc10c6621933fd05812f8297
Matthias Rolke <mr.amtrack@gmail.com>
2012-11-06 20:03:34 +0100

    small fixes

--- mavensmate.py
+++ mavensmate.py
@@ -11,7 +11,7 @@ import ast
 import copy
 if os.name != 'nt':
     import unicodedata
-import unicodedata, re
+import re
 import urllib
 from xml.dom.minidom import parse, parseString
 import json
@@ -59,8 +59,8 @@ def start_local_server():
     os.system(cmd)
 
 def stop_local_server():
-    cmd_b = ruby+" -r '"+mm_dir+"/support/lib/local_server.rb' -e 'MavensMate::LocalServer.stop'"
-    os.system(cmd)
+    cmd = ruby+" -r '"+mm_dir+"/support/lib/local_server.rb' -e 'MavensMate::LocalServer.stop'"
+    subprocess.Popen(cmd, shell=True)
 
 def generate_ui(ruby_script, args):
     p = subprocess.Popen(ruby+" '"+mm_dir+"/commands/"+ruby_script+".rb' "+args+"", stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
@@ -514,7 +514,7 @@ class MavensMateCompletions(sublime_plugin.EventListener):
             elif os.path.isfile(mm_project_directory()+"/src/classes/"+word+".cls"): #=> custom apex class static methods
                 search_name = prep_for_search(word)
                 print search_name
-                print 'looking for class def in: ' + mm_project_directory()+"/config/.class_docs/xml/class_"+search_name+".xml"
+                print 'looking for class def in: ' + mm_project_directory()+"/config/.class_docs/xml/"+search_name+".xml"
                 if os.path.isfile(mm_project_directory()+"/config/.class_docs/xml/"+search_name+".xml"):
                     object_dom = parse(mm_project_directory()+"/config/.class_docs/xml/"+search_name+".xml")
                     for node in object_dom.getElementsByTagName('memberdef'):
