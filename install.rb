b87d1edb728bbef2d3e14fc62651334fcbb61d1a
Matthias Rolke <mr.amtrack@gmail.com>
2012-11-07 13:17:46 +0100

    make install script work for windows and linux

--- install.rb
+++ install.rb
@@ -25,6 +25,9 @@ if (OS.mac?) then
 elsif (OS.linux?) then
   @install_path = File.expand_path("~/.config/sublime-text-2/Packages/MavensMate")
   @user_settings_path = File.expand_path("~/.config/sublime-text-2/Packages/User")
+elsif (OS.windows?) then
+  @install_path = File.expand_path("%USERPROFILE%/AppData/Roaming/Sublime Text 2/Packages/MavensMate")
+  @user_settings_path = File.expand_path("%USERPROFILE%/AppData/Roaming/Sublime Text 2/Packages/User")
 end
 
 def install_package
@@ -33,7 +36,7 @@ end
 
 def install_user_settings
   #update_user_config if File.exist?("#{@user_settings_path}/mavensmate.sublime-settings"
-  `cp '#{@install_path}/mavensmate.sublime-settings' '#{@user_settings_path}'` unless File.exist?("#{@user_settings_path}/mavensmate.sublime-settings")
+  FileUtils.copy("#{@install_path}/mavensmate.sublime-settings", @user_settings_path) unless File.exist?("#{@user_settings_path}/mavensmate.sublime-settings")
 end
 
 # def update_user_config
@@ -62,15 +65,17 @@ def install
 	elsif OS.mac?
     install_package
     install_user_settings
-  elsif OS.linux?
-    %x{pip install gkeyring}
+  elsif OS.linux? or OS.windows?
+    %x{pip install keyring}
     install_package
     install_user_settings
+  else
+    puts 'Could not install MavensMate. Your Operating System is not supported.'
 	end
 end
 
 def uninstall
-	`rm -rf '#{@install_path}'`
+  FileUtils.rm_rf(@install_path)
 end
 
 uninstall
