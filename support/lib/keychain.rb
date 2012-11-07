f591285272414e665cdf12935eff9b4df3505308
Matthias Rolke <mr.amtrack@gmail.com>
2012-11-07 18:34:40 +0100

    fix call of keyring-cli.py. didn't work on windows

--- support/lib/keychain.rb
+++ support/lib/keychain.rb
@@ -8,7 +8,7 @@ module KeyChain
       if MavensMate::OS.mac? then
         %x{security add-generic-password -a #{account} -s \"MavensMate: #{service}\" -w #{password} -U}
       elsif MavensMate::OS.linux? or MavensMate::OS.windows? then
-        %x("#{ROOT}/bin/keyring-cli.py" set "MavensMate: #{service}" "#{account}" "#{password}")
+        %x(python "#{ROOT}/bin/keyring-cli.py" set "MavensMate: #{service}" "#{account}" "#{password}")
       end
     end
     def find_generic_password(account, service)
@@ -16,14 +16,14 @@ module KeyChain
         result = %x{security find-generic-password -ga "#{account}" -s \"MavensMate: #{service}\" 2>&1 >/dev/null}
         result =~ /^password: "(.*)"$/ ? $1 : nil
       elsif MavensMate::OS.linux? or MavensMate::OS.windows? then
-        %x("#{ROOT}/bin/keyring-cli.py" get "MavensMate: #{service}" "#{account}").strip
+        %x(python "#{ROOT}/bin/keyring-cli.py" get "MavensMate: #{service}" "#{account}").strip
       end
     end
     def add_internet_password(user, proto, host, path, pass)
       if MavensMate::OS.mac? then
         %x{security add-internet-password -a "#{user}" -s "#{host}" -r "#{proto}" -p "#{path}" -w "#{pass}"}
       elsif MavensMate::OS.linux? or MavensMate::OS.windows? then
-        %x("#{ROOT}/bin/keyring-cli.py" set "MavensMate: #{service}" "#{account}" "#{pass}")
+        %x(python "#{ROOT}/bin/keyring-cli.py" set "MavensMate: #{service}" "#{account}" "#{pass}")
       end
     end
     def find_internet_password(aname)
@@ -31,7 +31,7 @@ module KeyChain
         result = %x{security find-generic-password -ga "#{aname}" 2>&1 >/dev/null}
         result =~ /^password: "(.*)"$/ ? $1 : nil
       elsif MavensMate::OS.linux? or MavensMate::OS.windows? then
-        %x("#{ROOT}/bin/keyring-cli.py" get "MavensMate: #{aname}" "#{aname}").strip
+        %x(python "#{ROOT}/bin/keyring-cli.py" get "MavensMate: #{aname}" "#{aname}").strip
       end
     end
   end
