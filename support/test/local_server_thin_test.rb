506ec2ed2dded97c5d013ea16df4d98989d6a4eb
Matthias Rolke <mr.amtrack@gmail.com>
2013-02-02 13:58:00 +0100

    merge ruby unit tests from @nwallace

--- support/test/local_server_thin_test.rb
+++ support/test/local_server_thin_test.rb
@@ -11,14 +11,64 @@ require LIB_ROOT + '/local_server_thin.rb'
 
 
 class LocalServerTests < Test::Unit::TestCase
-  def test_start_stop
-    MavensMate::LocalServerThin::stop
-    assert !(Lsof::running? 7777)
-    pid = Process.spawn("ruby -r #{ENV['TM_BUNDLE_SUPPORT']}/lib/local_server_thin.rb -e 'MavensMate::LocalServerThin.start'", :out => '/dev/null')
-    Process.detach pid
-    sleep 1
-    assert Lsof::running? 7777
-    MavensMate::LocalServerThin::stop
-    assert !(Lsof::running? 7777)
-  end
+    def setup
+        start_server
+    end
+
+    def teardown
+        stop_server
+    end
+
+    def test_start_stop
+        assert Lsof::running? 7777
+        stop_server
+        assert !(Lsof::running? 7777)
+        stop_server
+        assert !(Lsof::running? 7777)
+        start_server
+        assert Lsof::running? 7777
+    end
+
+    def test_respond
+        body_contents = "Test Body Contents"
+        content_type = "Test Content Type"
+        response = MavensMate::LocalServerThin.respond(body_contents, content_type)
+        assert response.include?(200), "response code should be 200"
+        assert response.include?(body_contents), "response should contain body contents"
+        assert response.to_s.include?(content_type), "response should have correct content type"
+    end
+
+    def test_respond_with_async_request_id
+        id = 42
+        response = MavensMate::LocalServerThin.respond_with_async_request_id(id)
+        assert response.is_a?(Array), "response should be an array"
+        assert response.to_s.include?(id.to_s), "response should contain the correct request id"
+    end
+
+    def test_prepare_response
+        options = {
+            :status  => "test status",
+            :success => "test success",
+            :id      => "test id",
+            :foo     => "foo"
+        }
+        response = MavensMate::LocalServerThin.prepare_response(options)
+        assert response.is_a?(String)
+        assert response.include?(options[:status]), "response should include correct status"
+        assert response.include?(options[:success]), "response should include correct success"
+        assert response.include?(options[:id]), "response should include correct id"
+        assert !response.include?(options[:foo]), "response should not include invalid options"
+    end
+
+    private
+
+    def start_server
+        pid = Process.spawn("ruby -r #{ENV['TM_BUNDLE_SUPPORT']}/lib/local_server_thin.rb -e 'MavensMate::LocalServerThin.start'", :out => '/dev/null')
+        Process.detach pid
+        sleep 1
+    end
+
+    def stop_server
+        MavensMate::LocalServerThin::stop
+    end
 end
