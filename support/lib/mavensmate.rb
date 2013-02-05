cbb05ed987b186ea5c7567948291cdb623c78dc3
Matthias Rolke <mr.amtrack@gmail.com>
2013-02-05 12:44:10 +0100

    possibly fix #84

--- support/lib/mavensmate.rb
+++ support/lib/mavensmate.rb
@@ -787,7 +787,7 @@ module MavensMate
     begin
       pconfig = MavensMate.get_project_config
       pconfig['org_connections'].each do |connection| 
-        pw = KeyChain::find_internet_password("#{pconfig['project_name']}-mm-#{connection['name']}")
+        pw = KeyChain::find_internet_password("#{pconfig['project_name']}-mm-#{connection['username']}")
         connections.push({
           :un => connection["username"], 
           :pw => pw
